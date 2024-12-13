f074959618c6f4b7f7559ba33eb02ace26212845
Sarab160 <142167410+Sarab160@users.noreply.github.com>
2024-12-13 10:20:56 -0800

    simple program

--- hello.py
+++ hello.py
@@ -16,3 +16,25 @@ elif action=="-":
     print(var-var1)
 else :
     print("Invalid action")       
+
+print("Match case ------")
+number= int(input("Enter the number"))
+
+match number:
+    case 1:
+        print("Monday")
+    case 2:
+        print("Tuesday")
+    case 3:
+        print("Wednesday")
+    case 4:
+        print("Thursday")
+    case 5:
+        print("Friday")
+    case 6:
+        print("Saturday")
+    case 7:
+        print("Sunday")
+    case _:
+        print("Default input")
+        
